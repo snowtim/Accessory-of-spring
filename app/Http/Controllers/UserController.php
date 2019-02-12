2e3fc7ccc88e41794ece1ab2ca9a6ce0bb55e8fa
Your Name <you@example.com>
2019-02-12 15:50:53 +0800

    Flower of spring
    usercontroller@store and index.blade.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index() {
+
+    	return view('User.userinfo');
+
+    }
+
+    public function store(Request $request) {
+
+     	$validator = Validator::make($request->all(),[
+    		'user_id' => 'required|string',
+    		'name' => 'required|string|max:255',
+    		'email' => 'required|string|max:255',
+    		'sex' => 'required|boolean',
+    		'birthday' => 'required|string',
+    		'phone' => 'required|string',
+    		'address' => 'required|string|max:255',
+    		'password' => 'required|string'
+    	]);	
+    	if($validator->fails()) {
+    		echo "Check the information you key in.";
+    	}
+
+    	User::create([
+    		'user_id' => request('user_id'),
+    		'name' => request('name'),
+    		'email' => request('email'),
+    		'sex' => request('sex'),
+    		'birthday' => request('birthday'),
+    		'phone' => request('phone'),
+    		'address' => request('address'),
+    		'password' => request('password')
+    	]);
+
+    	return redirect('/userinfo');
+    }
+
+    public function update(Request $request) {
+
+    	//$result = User::where('user_id', '=', $request('user_id'));
+    	$user = User::where('user_id', '=', $request('user_id'))
+    			->update();
+    	//$result->update();	
+
+    }
+
+    public function delete() {
+
+    }
+}
---
+++ resources/views/User/userinfo.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+	<html>
+
+	<head>
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<h1>Register your information</h1>
+			
+		<form method="POST">
+
+			<div>
+				<input type="text" name="user_id" placeholder="Your user ID">
+			</div>
+
+			<div>
+				<input type="text" name="name" placeholder="Your name">
+			</div>
+
+			<div>
+				<input type="text" name="email" placeholder="Your email">
+			</div>
+
+			<div>
+				<input type="" name="sex">
+			</div>
+
+			<div>
+				<input type="text" name="birthday" placeholder="Your birthday">
+			</div>
+
+			<div>
+				<input type="text" name="phone" placeholder="Your phone">
+			</div>
+
+			<div>
+				<input type="text" name="address" placeholder="Your address">
+			</div>
+
+			<div>
+				<input type="text" name="password" placeholder="password">
+			</div>
+
+			<div>
+				<button type="submit">Send your information</button>
+			</div>
+
+	</body>
+
+</form>
