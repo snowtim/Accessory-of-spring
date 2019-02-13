fcf7e14da4c4fabf8b6e3ab93ee3b0dbb49ce04f
Your Name <you@example.com>
2019-02-13 17:46:24 +0800

    Flower of spring
    product ：controller part

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,15 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
-{
-	public function create() {
+{	
+	/*protected function validator(array $productdata) {
+		return Validator::make($productdate, [
+			'product_id' => ['required', 'string', 'max:255'],
+			'product_name' => ['required', 'string', 'max:255'],
+			'description' => ['required', 'string', 'max:255'],
+			'price' => ['required', 'string']
+		]);
+	}*/
 
+	public function create(Request $request) {
+
+		$validator = Validator::make($request->all(), [
+			'product_id' => ['required', 'string', 'max:255'],
+			'product_name' => ['required', 'string', 'max:255'],
+			'description' => ['required', 'string', 'max:255'],
+			'price' => ['required', 'string']
+		]);
+
+		return Product::create([
+			'product_id' => $productdata['product_id'],
+			'product_name' => $productdata['product_name'],
+			'description' => $productdata['description'],	
+			'price' => $productdata['price']
+		]);
 	}
 
 	public function show() {
-
+		//return redirect('/product')
 	}
 
 	public function delete() {
@@ -23,6 +46,6 @@ class ProductController extends Controller
 	}
 
 	public function productindex() {
-		
+
 	}
 }
